0157c9fe49e7539154981f85a05b5aae0bd07832
zhrputriaz <zhrputriaz@gmail.com>
2024-11-26 09:02:16 +0700

    P6

--- test/latihan.test.js
+++ test/latihan.test.js
@@ -3,3 +3,51 @@ import { Counter, Greeting, AlertButton } from './latihan';
 import '@testing-library/jest-dom';
 import React from 'react';
 
+test('Counter Default Value must be 0', () => {
+    render(<Counter />);
+    const countValue = screen.getByTestId('counter-value');
+    expect(countValue).toHaveTextContent('0');
+});
+
+test('increment works when button clicked', () => {
+    render(<Counter />);
+    const countValue = screen.getByTestId('counter-value');
+    const incrementButton = screen.getByTestId('increment-button');
+    fireEvent.click(incrementButton);
+    expect(countValue).toHaveTextContent('1');
+});
+
+test('decrement works when button clicked', () => {
+    render(<Counter />);
+    const countValue = screen.getByTestId('counter-value');
+    const decrementButton = screen.getByTestId('decrement-button');
+    fireEvent.click(decrementButton);
+    expect(countValue).toHaveTextContent('-1');
+});
+
+test('reset the count using reset button', () => {
+    render(<Counter />);
+    const countValue = screen.getByTestId('counter-value');
+    const incrementButton = screen.getByTestId('increment-button');
+    const resetButton = screen.getByTestId('reset-button');
+
+    fireEvent.click(incrementButton); 
+    fireEvent.click(resetButton); 
+    expect(countValue).toHaveTextContent('0');
+});
+
+test('greeting component renders {nama}', () => {
+    render(<Greeting name="Zahrap" />);
+    const greetingMessage = screen.getByTestId('greeting');
+    expect(greetingMessage).toHaveTextContent('Hello, Zahrap');
+});
+
+
+test('triggers alert with correct message when clicked', () => {
+    window.alert = jest.fn(); 
+    render(<AlertButton message="Hello, Zahrap!" />);
+    const button = screen.getByTestId('alert-button');
+    fireEvent.click(button);
+    expect(window.alert).toHaveBeenCalledWith('Hello, Zahrap!');
+});
+
